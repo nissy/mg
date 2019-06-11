6d4ba742b1a03926a38dea75d50edfa6bd822014
nissy <ni@sheeda.jp>
2019-06-11 15:36:30 +0900

    fix version

--- mg_test.go
+++ mg_test.go
@@ -1,1 +1,15 @@
 package mg
+
+import "testing"
+
+func TestFileNameToVersion(t *testing.T) {
+	for version, filename := range map[uint64]string{
+		1:   "0000001.sql",
+		200: "200_aaa.sql",
+		111: "111aaa.sql",
+	} {
+		if v, err := FileNameToVersion(filename); err != nil || version != v {
+			t.Fatalf("FileName=%s, Version=%d, WantVersion=%d", filename, version, v)
+		}
+	}
+}
