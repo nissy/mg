93b15db5c50fbab5b5995e5ada0a393d28500529
nissy <ni@sheeda.jp>
2019-06-29 18:57:47 +0900

    fix mysql

--- version.go
+++ version.go
@@ -1,10 +1,9 @@
 package mg
 
 import (
-	"database/sql"
 	"fmt"
 
-	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
 )
 
@@ -19,26 +18,11 @@ type (
 	vPostgres struct {
 		table string
 	}
+
 	vMySQL struct {
 		table string
 	}
 )
-
-func openDatabase(driver, dsn string) (*sql.DB, error) {
-	switch driver {
-	case "postgres":
-	case "mysql":
-		cfg, err := mysql.ParseDSN(dsn)
-		if err != nil {
-			return nil, err
-		}
-		cfg.MultiStatements = true
-		cfg.ParseTime = true
-		dsn = cfg.FormatDSN()
-	}
-
-	return sql.Open(driver, dsn)
-}
 
 func FetchVersionSQLBuilder(driver, table string) VersionSQLBuilder {
 	switch driver {
